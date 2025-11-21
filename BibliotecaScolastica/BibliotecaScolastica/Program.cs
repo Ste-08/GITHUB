6ded5e665666d0ef0474857274abb75b4c579caa
Stefano Cozzula <STEFANO.COZZULA@local.ittcesena.it>
2025-11-21 09:31:01 +0100

    R0.1: prima versione di program.cs

--- BibliotecaScolastica/BibliotecaScolastica/Program.cs
+++ BibliotecaScolastica/BibliotecaScolastica/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool ancoraLibro = false;
+        Console.WriteLine("Benvenuto nella biblioteca");
+        Console.WriteLine("Stabilire l'indirizzo della biblioteca:");
+        string indirizzo = Console.ReadLine();
+        Console.WriteLine("Inserire il nome della biblioteca: ");
+        string nomeBiblioteca = Console.ReadLine();
+        Console.WriteLine("Inserire l'orario di apertura: ");
+        int orarioApertura = int.Parse(Console.ReadLine());
+        Console.WriteLine("Inserire l'orario di chiusura: ");
+        int orariochiusura = int.Parse(Console.ReadLine());
+        do
+        {
+            Console.WriteLine("Inserisci il nome del libro: ");
+            string nomeLibro = Console.ReadLine();
+
+        } while (ancoraLibro);
+
+    }
+}
